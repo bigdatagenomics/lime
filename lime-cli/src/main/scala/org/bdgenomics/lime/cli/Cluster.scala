ac7daa1c5418142457ed0c19c77be08761b8e0c1
Michael Heuer <heuermh@acm.org>
2018-02-22 11:26:13 -0600

    Use loadFeatures, writeTextRdd for cluster.

--- lime-cli/src/main/scala/org/bdgenomics/lime/cli/Cluster.scala
+++ lime-cli/src/main/scala/org/bdgenomics/lime/cli/Cluster.scala
@@ -19,10 +19,10 @@ package org.bdgenomics.lime.cli
 
 import org.apache.spark.SparkContext
 import org.bdgenomics.adam.rdd.ADAMContext._
-import org.bdgenomics.adam.rdd.ADAMSaveAnyArgs
+import org.bdgenomics.adam.util.TextRddWriter
 import org.bdgenomics.lime.op.ShuffleCluster
 import org.bdgenomics.utils.cli._
-import org.kohsuke.args4j.Argument
+import org.kohsuke.args4j.{ Argument, Option ⇒ Args4jOption }
 
 object Cluster extends BDGCommandCompanion {
   val commandName = "cluster"
@@ -32,31 +32,57 @@ object Cluster extends BDGCommandCompanion {
     new Cluster(Args4j[ClusterArgs](cmdLine))
   }
 
-  class ClusterArgs extends Args4jBase with ADAMSaveAnyArgs with ParquetArgs {
+  class ClusterArgs extends Args4jBase with ParquetSaveArgs {
     @Argument(required = true,
       metaVar = "INPUT",
-      usage = "The input file for cluster",
+      usage = "The features file to cluster (e.g., .bed, .gff/.gtf, .gff3, .interval_list, .narrowPeak). If extension is not detected, Parquet is assumed.",
       index = 0)
     var input: String = null
 
-    override var sortFastqOutput: Boolean = false
-    override var asSingleFile: Boolean = false
-    override var deferMerging: Boolean = false
-    override var outputPath: String = ""
-    override var disableFastConcat: Boolean = false
+    @Argument(required = false,
+      metaVar = "OUTPUT",
+      usage = "The output file for cluster, if any. If not specified, cluster will collect on the driver node and write to stdout.",
+      index = 1)
+    var outputPath: String = null
+
+    @Args4jOption(required = false,
+      name = "-num_partitions",
+      usage = "Number of partitions to use when loading the features file.")
+    var numPartitions: Int = _
+
+    @Args4jOption(required = false,
+      name = "-single",
+      usage = "Save as a single text file.")
+    var asSingleFile: Boolean = false
+
+    @Args4jOption(required = false,
+      name = "-disable_fast_concat",
+      usage = "Disables the parallel file concatenation engine.")
+    var disableFastConcat: Boolean = false
   }
 
   class Cluster(protected val args: ClusterArgs) extends BDGSparkCommand[ClusterArgs] {
     val companion = Intersection
 
     def run(sc: SparkContext) {
-      val leftGenomicRDD = sc.loadBed(args.input)
+      val leftGenomicRDD = sc.loadFeatures(
+        args.input,
+        optMinPartitions = Option(args.numPartitions),
+        optProjection = None)
 
-      ShuffleCluster(leftGenomicRDD)
+      val clusterRDD = ShuffleCluster(leftGenomicRDD)
         .compute()
         .rdd
-        .collect
-        .foreach(println)
+
+      Option(args.outputPath).fold(
+        clusterRDD
+          .collect
+          .foreach(println))(
+          TextRddWriter.writeTextRdd(
+            clusterRDD,
+            _,
+            asSingleFile = args.asSingleFile,
+            disableFastConcat = args.disableFastConcat))
     }
   }
 }
